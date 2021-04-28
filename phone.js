b224d07d0c33445bccccfe4f4df1d9576c04dc97
Gilberto-007 <78499550+Gilberto-007@users.noreply.github.com>
2021-04-28 19:18:07 -0400

    Update phone.js

--- phone.js
+++ phone.js
@@ -1,14 +1,5 @@
 
-var phone_num = {
-"start": "Here is your phone number.",
-"area_code" : ["787", "939"],
-"num" : ["1","2","3","4","5","6","7","8","9","10"],
-"first_name" : ["Gilberto","Diego","Miguel","Dylan"],
-"last_name" : ["Cancel","Pérez","Hernandez","Cedrés"],
-"phone" : "#first_name# #last_name#: (#area_code#)#num##num##num##num##num##num##num#"
-}
 
-var grammar;
 
 function setup(){
 

dfef168975c4187947fff2de19a64c6fe6eb7ae3
Gilberto-007 <78499550+Gilberto-007@users.noreply.github.com>
2021-04-28 13:56:56 -0400

    Create phone.js

---
+++ phone.js
@@ -0,0 +1,20 @@
+var phone_num{
+"start": "Here is your phone number.",
+"area_code" : ["787", "939"],
+"num" : ["1","2","3","4","5","6","7","8","9","10"],
+"first_name" : ["Gilberto","Diego","Miguel","Dylan"],
+"last_name" : ["Cancel","Pérez","Hernandez","Cedrés"]
+}
+
+var grammar;
+
+function setup()
+noCanvas();
+
+grammer = tracery.createGrammar(phone_num)
+
+var result = grammar.flatten(#start#);
+console.log(result);
+
+
+

d9aebf998456ae4bf51abf3a951639d5075596e3
Gilberto-007 <78499550+Gilberto-007@users.noreply.github.com>
2021-04-28 19:27:30 -0400

    Update grammar.js

--- grammar.js
+++ grammar.js
@@ -1,5 +1,7 @@
-var grammars = {
-phone_num : {
+let sentences =[]
+
+function generate(){
+var phone_num = {
 "start": "Here is your phone number.",
 "area_code" : ["787", "939"],
 "num" : ["1","2","3","4","5","6","7","8","9","10"],
@@ -7,4 +9,24 @@ phone_num : {
 "last_name" : ["Cancel","Pérez","Hernandez","Cedrés"],
 "phone" : "#first_name# #last_name#: (#area_code#)#num##num##num##num##num##num##num#"
   }
+
+let grammar = tracery.createGrammar(phone_num);
+
+let expansion = grammar.flatten('#phone');
+sentences.push(expansion);
+  
+printSentences(sentences);
 }
+
+function printSentences(sentences) {
+    let textBox = document.getElementById("sentences");
+    textBox.innerHTML = "";
+    for(let i=sentences.length-1; i>=0; i--) {
+        textBox.innerHTML += "<p>"+sentences[i]+"</p>"
+    }
+}
+  
+
+
+
+

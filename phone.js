80556a95477448024202b998d42957bdcd74f002
Gilberto-007 <78499550+Gilberto-007@users.noreply.github.com>
2021-04-28 14:12:55 -0400

    Update phone.js

--- phone.js
+++ phone.js
@@ -3,7 +3,8 @@ var phone_num = {
 "area_code" : ["787", "939"],
 "num" : ["1","2","3","4","5","6","7","8","9","10"],
 "first_name" : ["Gilberto","Diego","Miguel","Dylan"],
-"last_name" : ["Cancel","Pérez","Hernandez","Cedrés"]
+"last_name" : ["Cancel","Pérez","Hernandez","Cedrés"],
+"phone_num" : "#first_name# #last_name#: (#area_code#)#num##num##num##num##num##num##num#"
 }
 
 var grammar;

01a522863b18909065169cb1ec22ccfb5b42c704
Gilberto-007 <78499550+Gilberto-007@users.noreply.github.com>
2021-04-28 19:32:06 -0400

    Update grammar.js

--- grammar.js
+++ grammar.js
@@ -7,7 +7,7 @@ var phone_num = {
 "num" : ["1","2","3","4","5","6","7","8","9","10"],
 "first_name" : ["Gilberto","Diego","Miguel","Dylan"],
 "last_name" : ["Cancel","Pérez","Hernandez","Cedrés"],
-"phone" : "#first_name# #last_name#: (#area_code#)#num##num##num# - #num##num##num##num#"
+"phone" : "#first_name# #last_name#: (#area_code#)#num##num##num#-#num##num##num##num#"
   }
 
 let grammar = tracery.createGrammar(phone_num);

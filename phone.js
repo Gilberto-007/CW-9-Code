a690deb315184b56b3001da7fd0137f5c5ee1110
Gilberto-007 <78499550+Gilberto-007@users.noreply.github.com>
2021-04-28 14:13:54 -0400

    Update phone.js

--- phone.js
+++ phone.js
@@ -4,7 +4,7 @@ var phone_num = {
 "num" : ["1","2","3","4","5","6","7","8","9","10"],
 "first_name" : ["Gilberto","Diego","Miguel","Dylan"],
 "last_name" : ["Cancel","Pérez","Hernandez","Cedrés"],
-"phone_num" : "#first_name# #last_name#: (#area_code#)#num##num##num##num##num##num##num#"
+"phone" : "#first_name# #last_name#: (#area_code#)#num##num##num##num##num##num##num#"
 }
 
 var grammar;
@@ -12,7 +12,7 @@ var grammar;
 function setup(){
 noCanvas();
 
-grammer = tracery.createGrammar(phone_num);
+grammer = tracery.createGrammar("#phone#");
 
 var result = grammar.flatten(#phone_num#);
 console.log(result);

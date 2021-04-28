54e6343e96bf91c08e832cebd8ed28ab8060f266
Gilberto-007 <78499550+Gilberto-007@users.noreply.github.com>
2021-04-28 19:24:51 -0400

    Update phone.js

--- phone.js
+++ phone.js
@@ -1,13 +1,24 @@
 
+let sentences =[]
 
+function generate(){
 
-function setup(){
+let grammar = tracery.createGrammar(phone_num);
 
-
-grammer = tracery.createGrammar(phone_num);
-
-var result = grammar.flatten("#phone");
-console.log(result);
+let expansion = grammar.flatten('#phone');
+sentences.push(expansion);
+  
+printSentences(sentences);
 }
+
+function printSentences(sentences) {
+    let textBox = document.getElementById("sentences");
+    textBox.innerHTML = "";
+    for(let i=sentences.length-1; i>=0; i--) {
+        textBox.innerHTML += "<p>"+sentences[i]+"</p>"
+    }
+}
+  
+
 
 

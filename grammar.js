3833dfe2a80a94d033a660f969e9a1d6e60c8459
Gilberto-007 <78499550+Gilberto-007@users.noreply.github.com>
2021-05-03 13:49:44 -0400

    Update grammar.js

--- grammar.js
+++ grammar.js
@@ -4,7 +4,7 @@ function generate(){
 var phone_num = {
 "start": "Here is your phone number.",
 "area_code" : ["787", "939"],
-"num" : ["1","2","3","4","5","6","7","8","9","10"],
+"num" : ["1","2","3","4","5","6","7","8","9","0"],
 "first_name" : ["Gilberto","Diego","Miguel","Dylan"],
 "last_name" : ["Cancel","Pérez","Hernandez","Cedrés"],
 "phone" : "#first_name# #last_name# #last_name# : (#area_code#)#num##num##num#-#num##num##num##num#"
